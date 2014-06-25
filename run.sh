f9b549a6882dbe74a036db1eeee16beda99e371a
renbo <justnow.ren@gmail>
2014-06-25 18:20:53 +0900

    revert run.sh

--- run.sh
+++ run.sh
@@ -68,38 +68,38 @@ mct_ngauss=45000
 
 if $do_prep; then
 
-# # Prepare clean data and language model.
-# local/wsj0cam_data_prep.sh $wsj0cam $reverb_lm || exit 1
+# Prepare clean data and language model.
+local/wsj0cam_data_prep.sh $wsj0cam $reverb_lm || exit 1
 
-# # Prepare merged BEEP/CMU dictionary.
-# local/wsj_prepare_beep_dict.sh || exit 1;
+# Prepare merged BEEP/CMU dictionary.
+local/wsj_prepare_beep_dict.sh || exit 1;
 
-# # Prepare wordlists, etc.
-# utils/prepare_lang.sh data/local/dict "<SPOKEN_NOISE>" data/local/lang_tmp data/lang || exit 1;
+# Prepare wordlists, etc.
+utils/prepare_lang.sh data/local/dict "<SPOKEN_NOISE>" data/local/lang_tmp data/lang || exit 1;
 
-# # Prepare directory structure for clean data. Apply some language model fixes.
-# local/wsjcam0_format_data.sh || exit 1;
+# Prepare directory structure for clean data. Apply some language model fixes.
+local/wsjcam0_format_data.sh || exit 1;
 
-# # Now it's getting more interesting.
-# # Prepare the multi-condition training data and the REVERB dt set.
-# # This also extracts MFCC features.
-# # This creates the data sets called REVERB_tr_cut and REVERB_dt.
-# # If you have processed waveforms, this is a good starting point to integrate them.
-# # For example, you could have something like
-# # local/REVERB_wsjcam0_data_prep.sh /path/to/processed/REVERB_WSJCAM0_dt processed_REVERB_dt dt
-# # The first argument is supposed to point to a folder that has the same structure
-# # as the REVERB corpus.
-# local/REVERB_wsjcam0_data_prep.sh $reverb_tr REVERB_tr_cut tr || exit 1;
-# local/REVERB_wsjcam0_data_prep.sh $reverb_dt REVERB_dt dt     || exit 1;
-# #local/REVERB_wsjcam0_data_prep.sh $reverb_et REVERB_et et     || exit 1;
+# Now it's getting more interesting.
+# Prepare the multi-condition training data and the REVERB dt set.
+# This also extracts MFCC features.
+# This creates the data sets called REVERB_tr_cut and REVERB_dt.
+# If you have processed waveforms, this is a good starting point to integrate them.
+# For example, you could have something like
+# local/REVERB_wsjcam0_data_prep.sh /path/to/processed/REVERB_WSJCAM0_dt processed_REVERB_dt dt
+# The first argument is supposed to point to a folder that has the same structure
+# as the REVERB corpus.
+local/REVERB_wsjcam0_data_prep.sh $reverb_tr REVERB_tr_cut tr || exit 1;
+local/REVERB_wsjcam0_data_prep.sh $reverb_dt REVERB_dt dt     || exit 1;
+local/REVERB_wsjcam0_data_prep.sh $reverb_et REVERB_et et     || exit 1;
 
-# # Prepare the REVERB "real" dt set from MCWSJAV corpus.
-# # This corpus is *never* used for training.
-# # This creates the data set called REVERB_Real_dt and its subfolders
-# local/REVERB_mcwsjav_data_prep.sh $reverb_real_dt REVERB_Real_dt dt || exit 1;
-# # The MLF file exists only once in the corpus, namely in the real_dt directory
-# # so we pass it as 4th argument
-# local/REVERB_mcwsjav_data_prep.sh $reverb_real_et REVERB_Real_et et $reverb_real_dt/mlf/WSJ.mlf || exit 1;
+# Prepare the REVERB "real" dt set from MCWSJAV corpus.
+# This corpus is *never* used for training.
+# This creates the data set called REVERB_Real_dt and its subfolders
+local/REVERB_mcwsjav_data_prep.sh $reverb_real_dt REVERB_Real_dt dt || exit 1;
+# The MLF file exists only once in the corpus, namely in the real_dt directory
+# so we pass it as 4th argument
+local/REVERB_mcwsjav_data_prep.sh $reverb_real_et REVERB_Real_et et $reverb_real_dt/mlf/WSJ.mlf || exit 1;
 
 
 # Extract MFCC features for clean sets.
