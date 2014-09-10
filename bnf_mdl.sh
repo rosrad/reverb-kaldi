d01373f22b48e514fa99f24d791d4fda1ad7fa13
renbo <justnow.ren@gmail>
2014-09-10 17:12:02 +0900

    update the main script for training BNF

--- bnf_mdl.sh
+++ bnf_mdl.sh
@@ -3,16 +3,16 @@
 . check.sh
 
 
-export TR_MDL="tri1_mc bnf bnf_mc"
+export TR_MDL="bnf_mc"
 export DT_MDL=${TR_MDL}
 export REG=""
 
 for feat in mfcc; do
     export FEAT_TYPE=$feat
-    utils/call.sh \
+    utils/call.sh --tag bnf_mdl \
         local/TrainAMs.sh
 done
 
-export DT="REVERB_tr_cut REVERB_dt PHONE_dt PHONE_SEL_dt si_tr si_dt"
+export DT="REVERB_tr_cut REVERB_dt PHONE_dt PHONE_SEL_dt si_tr"
 utils/call.sh \
     local/ExtractFeats.sh  bnf
